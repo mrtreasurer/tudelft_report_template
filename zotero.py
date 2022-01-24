df3a8893cc7379d35664c6d626f6f0b0b8d5d617
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2022-01-24 14:32:26 +0100

    add auto zotero download

---
+++ zotero.py
@@ -0,0 +1,11 @@
+import json
+
+from pyzotero import zotero
+from bibtexparser import dump
+
+with open("zotero.json") as f:
+    vars = json.load(f)
+
+zot = zotero.Zotero(vars["user_id"], "user", vars["api_key"])
+with open(vars["output"], "w") as f:
+    dump(zot.collection_items(vars["collection_id"], format="bibtex"), f)
